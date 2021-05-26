73b912d67b8279c0ac4f6ccf03033382e26ffcb2
waqar3626 <55612827+waqar3626@users.noreply.github.com>
2021-05-26 16:10:12 +0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PracticeWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
